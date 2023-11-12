2187b9a3d700632c8ce00e3aaed348d9c66a70c3
WarrenLee19 <13719507574@163.com>
2023-11-13 01:59:25 +0800

    add:project init

---
+++ packages/outline/src/index.ts
@@ -0,0 +1,6 @@
+function fun2() {
+    console.log('I am package 2');
+  }
+  
+  export default fun2;
+  
---
+++ playground/src/index.ts
@@ -0,0 +1,8 @@
+import pkg2 from 'lexical-outline';
+
+function fun2() {
+  pkg2();
+  console.log('I am package 1');
+}
+
+export default fun2;
